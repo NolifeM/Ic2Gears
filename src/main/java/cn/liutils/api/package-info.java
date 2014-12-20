ee0171dfc54d3b050a76a4183c25fe70f140c24c
NolifeM <panfengjason@163.com>
2014-12-20 22:25:19 +0800

    clear API code
    clear API code

--- src/main/java/cn/liutils/api/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author WeathFolD
- *
- */
-@API(owner = "LIUtils", provides = "LIUtilsAPI", apiVersion = cn.liutils.core.LIUtils.VERSION)
-package cn.liutils.api;
-import cpw.mods.fml.common.API;
